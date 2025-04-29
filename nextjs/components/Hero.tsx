5d5821d633b3b62b3e16c028ebf46da166297d07
Kevin Everrett <advancedeveloper24@gmail.com>
2025-04-29 19:23:31 -0400

    Hero section

---
+++ nextjs/components/Hero.tsx
@@ -0,0 +1,28 @@
+"use client";
+import Image from "next/image";
+import { CustomButton } from ".";
+
+const Hero = () => {
+  const handleScroll = () => {};
+
+  return (
+    <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold ">
+          Find, book, or rent a car - quickly and easily!
+        </h1>
+        <p className="text-[27px] text-black-100 font-light mt-5">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
